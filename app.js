7e021c8e61f8a8eae1c61bdcac54f8770d1561d0
Marie Angelene Cuevo <cuevoangel@gmail.com>
2023-09-22 16:41:36 +0800

    minor changes

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.post('/save', async (req, res) => {
             name,
         },
     });
-    res.json({ user });
+    res.json({ 
+        user,
+        message: 'User created successfully.'
+ });
 });
 
 // USING GET METHOD. Listens for endpoint "/user" then runs a function that retrieves user data atleast one or none.
